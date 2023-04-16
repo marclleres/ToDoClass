5137335a3db3bb7e5eadbcf88b3f5d073e76cb9e
Lleres Marc <marc.lleres@gmail.com>
2023-04-16 17:38:47 +0200

    add items

--- src/component/common/TheList.jsx
+++ src/component/common/TheList.jsx
@@ -1,29 +1,39 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const TheList = () => {
-    const [things, setThings] = useState([{}]);
-    const [addOpen, setAddOpen] = useState(false);
+    const [toDoItems, setToDoItem] = useState([{}]);
+    const [isAddItemOpen, setIsAddItemOpen] = useState(false);
 
     useEffect(() => {
-        setThings([
+        setToDoItem([
             { name: "Laver la voiture", category: "Corvé" },
             { name: "Nettoyer le salon", category: "Corvé" },
             { name: "Manger", category: "Plaisir" },
         ]);
     }, []);
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        setToDoItem([...toDoItems, Object.fromEntries(formData.entries())]);
+        setIsAddItemOpen(false);
+    }
+
     return <div className="text-center my-4" >
         <div style={{ maxWidth: "40em" }}>
             <h1 className="text-center my-4">To Do List</h1>
             <div className="d-flex justify-content-center">
-                <button type="button" className="btn btn-primary" onClick={() => setAddOpen(!addOpen)}>Add</button>
+                <button type="button" className="btn btn-outline-primary" onClick={() => setIsAddItemOpen(!isAddItemOpen)}>Add Item</button>
             </div>
-            {addOpen
-                ? <div>
-                    <input className="m-4"></input>
-                    <input className="m-4"></input>
-                </div>
+            {isAddItemOpen
+                ? <form onSubmit={handleSubmit} className="p-4">
+                    <div className="d-flex justify-content-center">
+                        <div className="p-2"><input name="name" placeholder="Name" required /></div>
+                        <div className="p-2"><input name="category" placeholder="Category" required /></div>
+                        <div className="p-2"><button type="reset" className="btn btn-outline-primary btn-sm" >Reset</button></div>
+                        <div className="p-2"><button type="submit" className="btn btn-outline-primary btn-sm" >Submit</button></div>
+                    </div>
+                </form>
                 : null}
             <div className="d-flex justify-content-center">
                 <table className="table table-striped mx-3 mx-lg-0">
@@ -35,15 +45,15 @@ export const TheList = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {things.map((thing) => {
-                            const myKey = `${thing.name}_${thing.category}`;
+                        {toDoItems.map((toDoItem) => {
+                            const myKey = `${toDoItem.name}_${toDoItem.category}`;
                             return (
                                 <tr key={myKey}>
                                     <th scope="row">
                                         <input className="form-check-input" type="checkbox" />
                                     </th>
-                                    <td>{thing.name}</td>
-                                    <td>{thing.category}</td>
+                                    <td>{toDoItem.name}</td>
+                                    <td>{toDoItem.category}</td>
                                 </tr>
                             )
                         })}
